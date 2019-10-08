3832c0eb734d8fdb37e4a3101a9eed6be7369a3a
jackLiu <1404432837@qq.com>
2019-10-08 17:08:03 +0800

    新增IRule负载均衡策略

--- consumer01-8080/src/main/java/com/example/consumer/Consumer018080Application.java
+++ consumer01-8080/src/main/java/com/example/consumer/Consumer018080Application.java
@@ -1,9 +1,12 @@
 package com.example.consumer;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @EnableEurekaClient
 @SpringBootApplication
@@ -14,4 +17,9 @@ public class Consumer018080Application {
         SpringApplication.run(Consumer018080Application.class, args);
     }
 
+
+    @Bean
+    public IRule loadBalanceRule() {
+        return new RandomRule();
+    }
 }
