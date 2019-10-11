b16c5fca8d8dc7224696d5b0cd62b7c70271c79f
jackLiu <1404432837@qq.com>
2019-10-11 11:08:27 +0800

    简单修改

--- provider/src/main/java/com/example/provider018081/Provider018081Application.java
+++ provider/src/main/java/com/example/provider018081/Provider018081Application.java
@@ -2,10 +2,12 @@ package com.example.provider018081;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Provider018081Application {
 
     public static void main(String[] args) {
