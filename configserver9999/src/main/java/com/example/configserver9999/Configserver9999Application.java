5276b9e0b01c238ef89663b01c69202abbdb7367
jackLiu <1404432837@qq.com>
2019-10-11 16:07:25 +0800

    新增配置中心

---
+++ configserver9999/src/main/java/com/example/configserver9999/Configserver9999Application.java
@@ -0,0 +1,13 @@
+package com.example.configserver9999;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Configserver9999Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Configserver9999Application.class, args);
+    }
+
+}
